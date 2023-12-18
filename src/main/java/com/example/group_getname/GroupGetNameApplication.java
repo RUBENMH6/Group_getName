fd43b1d896de37f82a2c58b3920b73cbad505afd
Ruben <alumar1999@ieselcaminas.org>
2023-12-18 15:21:50 +0100

    Primer commit

---
+++ src/main/java/com/example/group_getname/GroupGetNameApplication.java
@@ -0,0 +1,13 @@
+package com.example.group_getname;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupGetNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupGetNameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/group_getname/GroupGetNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.group_getname;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupGetNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
