26f1e2227e6c4304a7777eb1bd7246e074b6fd0e
Ruben <alumar1999@ieselcaminas.org>
2023-12-18 16:48:50 +0100

    Creación de la base de datos 'ddbb_clase'

--- src/main/java/com/example/group_getname/models/entity/Aula.java
+++ src/main/java/com/example/group_getname/models/entity/Aula.java
@@ -2,9 +2,10 @@ package com.example.group_getname.models.entity;
 
 
 import jakarta.persistence.*;
+import org.antlr.v4.runtime.misc.NotNull;
 
 @Entity
-@Table(name = "nadador")
+@Table(name="aula",uniqueConstraints = {@UniqueConstraint(columnNames = {"id_profesor", "id_curso", "id_horario"})})
 public class Aula {
 
     @Id
@@ -12,8 +13,21 @@ public class Aula {
     @Column(name = "id")
     private int id_aula;
 
+    @Column(name = "id_profesor")
+    private int id_profesor;
 
+    @NotNull
+    @Column(name = "id_curso")
+    private int id_curso;
 
+    @Column(name = "id_horario")
+    private int id_horario;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "color")
+    private String color;
     public void setId(int id_aula) {
         this.id_aula = id_aula;
     }
