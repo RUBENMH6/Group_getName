35435cc3573ec02f128b15368c1742a6f853614b
Ruben <alumar1999@ieselcaminas.org>
2023-12-20 19:35:55 +0100

    Admin DAO

---
+++ src/main/java/com/example/group_getname/models/dao/AdministradorDAO.java
@@ -0,0 +1,9 @@
+package com.example.group_getname.models.dao;
+
+import com.example.group_getname.models.entity.Administrador;
+import com.example.group_getname.models.entity.Aula;
+
+public interface AdministradorDAO {
+
+    public void save(Administrador administrador);
+}
---
+++ src/main/java/com/example/group_getname/models/dao/AdministradorDAOImpl.java
@@ -0,0 +1,23 @@
+package com.example.group_getname.models.dao;
+
+import com.example.group_getname.models.entity.Administrador;
+import com.example.group_getname.models.entity.Aula;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AdministradorDAOImpl implements AdministradorDAO{
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public AdministradorDAOImpl(EntityManager theEntityManager) {
+        entityManager = theEntityManager;
+    }
+
+    @Override
+    public void save(Administrador administrador) {
+        entityManager.persist(administrador);
+    }
+}
