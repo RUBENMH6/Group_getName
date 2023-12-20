fa03d64992fa663b5ef8eecc08667423bceb41f7
sebasogueta <sebasogueta@gmail.com>
2023-12-20 19:39:22 +0100

    clase profesor importadas en los DAO

--- src/main/java/com/example/group_getname/models/dao/ProfesorDAO.java
+++ src/main/java/com/example/group_getname/models/dao/ProfesorDAO.java
@@ -1,6 +1,7 @@
 package com.example.group_getname.models.dao;
 
 import com.example.group_getname.models.entity.Curso;
+import com.example.group_getname.models.entity.Profesor;
 
 public interface ProfesorDAO {
 
--- src/main/java/com/example/group_getname/models/dao/ProfesorDAOImpl.java
+++ src/main/java/com/example/group_getname/models/dao/ProfesorDAOImpl.java
@@ -1,6 +1,7 @@
 package com.example.group_getname.models.dao;
 
 import com.example.group_getname.models.entity.Curso;
+import com.example.group_getname.models.entity.Profesor;
 import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
