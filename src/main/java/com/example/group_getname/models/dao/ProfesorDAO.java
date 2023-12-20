1d5181333543196f17e757db50af3127c18b10b7
sebasogueta <sebasogueta@gmail.com>
2023-12-20 19:32:30 +0100

    profesor DAO y DAOimpl (falta calse Profesor)

--- src/main/java/com/example/group_getname/models/dao/ProfesorDAO.java
+++ src/main/java/com/example/group_getname/models/dao/ProfesorDAO.java
@@ -1,4 +1,10 @@
 package com.example.group_getname.models.dao;
 
+import com.example.group_getname.models.entity.Curso;
+
 public interface ProfesorDAO {
+
+    public void save(Profesor profesor);
+
 }
+
--- src/main/java/com/example/group_getname/models/dao/ProfesorDAOImpl.java
+++ src/main/java/com/example/group_getname/models/dao/ProfesorDAOImpl.java
@@ -1,4 +1,25 @@
 package com.example.group_getname.models.dao;
 
-public class ProfesorDAOImpl {
+import com.example.group_getname.models.entity.Curso;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class ProfesorDAOImpl implements ProfesorDAO{
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public ProfesorDAOImpl(EntityManager theEntityManager) {
+        entityManager = theEntityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Profesor profesor) {
+        entityManager.persist(profesor);
+    }
+
 }
