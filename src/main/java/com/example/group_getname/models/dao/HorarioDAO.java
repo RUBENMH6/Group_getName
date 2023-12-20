8b5bc822fb2fa8308720b45e51a8443ec676d38d
Ruben <alumar1999@ieselcaminas.org>
2023-12-20 19:41:14 +0100

    Horario acabado

--- src/main/java/com/example/group_getname/models/dao/HorarioDAO.java
+++ src/main/java/com/example/group_getname/models/dao/HorarioDAO.java
@@ -1,5 +1,8 @@
 package com.example.group_getname.models.dao;
 
+import com.example.group_getname.models.entity.Horario;
+
 public interface HorarioDAO {
 
+    public void save(Horario horario);
 }
--- src/main/java/com/example/group_getname/models/dao/HorarioDAOImpl.java
+++ src/main/java/com/example/group_getname/models/dao/HorarioDAOImpl.java
@@ -1,4 +1,24 @@
 package com.example.group_getname.models.dao;
 
-public class HorarioDAOImpl {
+import com.example.group_getname.models.entity.Administrador;
+import com.example.group_getname.models.entity.Curso;
+import com.example.group_getname.models.entity.Horario;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class HorarioDAOImpl implements HorarioDAO {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public HorarioDAOImpl(EntityManager theEntityManager) {
+        entityManager = theEntityManager;
+    }
+
+    @Override
+    public void save(Horario horario) {
+        entityManager.persist(horario);
+    }
 }
