c5f8f86f8af67133152797e03511294d6a128d86
Rubén Martínez Hidalgo <ruben.martinez@thenetfirm.com>
2023-12-24 18:09:44 +0100

    Asignatura a mitad

--- src/main/java/com/example/group_getname/models/dao/AsignaturaDAO.java
+++ src/main/java/com/example/group_getname/models/dao/AsignaturaDAO.java
@@ -1,8 +1,20 @@
 package com.example.group_getname.models.dao;
 
 import com.example.group_getname.models.entity.Asignatura;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+import java.util.List;
 
 public interface AsignaturaDAO {
 
-    public void save(Asignatura aula);
+    public void save(Asignatura asignatura);
+
+    public Asignatura getForId(int id);
+
+    public List<Asignatura> findAll();
+
+    public void updateName(Asignatura asignatura, String nombre);
+    public void updateIdProfesor(Asignatura asignatura, int idProf);
+    public void updateIdCurso(Asignatura asignatura, int idCurso);
+    public void updateIdHorario(Asignatura asignatura, int idHorario);
 }
