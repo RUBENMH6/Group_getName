4d9ea678294a57caa1d8fed6d57d0fcf7e1ba5a4
Ruben <alumar1999@ieselcaminas.org>
2024-01-18 17:05:30 +0100

    Fix repo

--- src/main/java/com/example/group_getname/repository/AsignaturaRepo.java
+++ src/main/java/com/example/group_getname/repository/AsignaturaRepo.java
@@ -2,6 +2,8 @@ package com.example.group_getname.repository;
 
 import com.example.group_getname.models.entity.Asignatura;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AsignaturaRepo extends JpaRepository<Asignatura, Integer> {
 }
